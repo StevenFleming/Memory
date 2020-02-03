50c16661ca650750807275352b83a08210550823
Andrew Philpott and Steven Fleming <andrew+steven@>
2020-02-03 13:59:45 -0800

    Fix logic for randomize Pairs.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+import './css/styles.css';
+import { defineCards } from './js/interfacelogic.js';
+import { randomizePairs } from './js/interfacelogic.js';
+import { displayPairs } from './js/interfacelogic.js';
+
+$(document).ready(function() {
+  let cards = defineCards(4);
+  let arr = randomizePairs(cards);
+  displayPairs(arr);
+});
--- src/js/interfacelogic.js
+++ src/js/interfacelogic.js
@@ -1,19 +1,32 @@
-function defineCards(numberOfCardPairs) {
-    let allCards = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-    let subCards = allCards.slice(0, numberOfCardPairs-1);
-    let doubleCards = subCards.concat(subCards);
-    return doubleCards;
+import $ from 'jquery';
+
+export function defineCards(numberOfCardPairs) {
+  let allCards = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+  let subCards = allCards.slice(0, numberOfCardPairs);
+  let doubleCards = subCards.concat(subCards);
+
+  return doubleCards;
 }
 
-function randomizePairs(selectionArray) {
-    let newArray = [];
-    let currentSelectionArrayLength = selectionArray.length;
-    let initialSelectionArrayLength = currentSelectionArrayLength;
+export function randomizePairs(selectionArray) {
+  let newArray = [];
+  let initialSelectionArrayLength = selectionArray.length;
 
-    for(let i = 0; i < initialSelectionArrayLength; i++) {
-        let value = Math.floor(Math.random() * (currentSelectionArrayLength-1));
-        newArray.push(selectionArray[value]);
-        selectionArray = selectionArray.slice(value);
-    }
-    return newArray;
+  for(let i = 0; i < initialSelectionArrayLength; i++) {
+      let currentSelectionArrayLength = selectionArray.length;
+      let value = Math.floor((Math.random() * (currentSelectionArrayLength-1)));
+      newArray.push(selectionArray[value]);
+      selectionArray.splice(value,1);
+
+  }
+  return newArray;
+}
+
+export function displayPairs(cards) {
+  let gameDisplay = $("#game-display");
+  let cardsHtml = ``;
+  for(let i = 0; i < cards.length; i++) {
+      cardsHtml += `<div class='card'><p>${cards[i]}</p></div>`;
+  }
+  return gameDisplay.html(cardsHtml);
 }
